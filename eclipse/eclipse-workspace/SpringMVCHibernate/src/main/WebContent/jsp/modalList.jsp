a6ee64e59661dc9483d688432afe9aa36e511fa3
chojaechun <jaechun0208@naver.com>
2017-12-20 18:12:01 +0900

    #JJC20171220-02 paging update

---
+++ eclipse/eclipse-workspace/SpringMVCHibernate/src/main/WebContent/jsp/modalList.jsp
@@ -0,0 +1,59 @@
+
+<style>
+  /* Note: Try to remove the following lines to see the effect of CSS positioning */
+  .affix {
+      top: 0;
+      width: 100%;
+  }
+
+  .affix + .container-fluid {
+      padding-top: 70px;
+  }
+  
+.glyphicon.glyphicon-picture{
+    font-size: 20px;
+}
+  .modal-header, h4, .close {
+      background-color: #5cb85c;
+      color:white !important;
+      text-align: center;
+      font-size: 30px;
+  }
+  .modal-footer {
+      background-color: #f9f9f9;
+  }
+
+</style>
+
+<div class="modal fade" id="" role="dialog">
+	<div class="modal-content">
+		<div class="modal-header" style="padding:35px 50px;">
+			<button type="button" class="close" data-dismiss="modal">&times;</button>
+			<h4><span class="glyphicon glyphicon-lock"></span> Login</h4>
+		</div>
+		<div class="modal-body" style="padding:40px 50px;">
+			<form role="form">
+				<div class="form-group">
+					<label for="usrname"><span class="glyphicon glyphicon-user"></span> Username</label>
+					<input type="text" class="form-control" id="usrname" placeholder="Enter email">
+				</div>
+				<div class="form-group">
+					<label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
+					<input type="text" class="form-control" id="psw" placeholder="Enter password">
+				</div>
+				<div class="checkbox">
+					<label><input type="checkbox" value="" checked>Remember me</label>
+				</div>
+				<button type="submit" class="btn btn-success btn-block"><span class="glyphicon glyphicon-off"></span> Login</button>
+			</form>
+        </div>
+		<div class="modal-footer">
+			<button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
+			<p>Not a member? <a href="#">Sign Up</a></p>
+			<p>Forgot <a href="#">Password?</a></p>
+		</div>
+	</div>
+</div>
+
+
+
---
+++ eclipse/eclipse-workspace/SpringMVCHibernate/src/main/java/com/spring/mvc/dao/Paging.java
@@ -0,0 +1,114 @@
+package com.spring.mvc.dao;
+
+public class Paging {
+	private int startCount;
+	private int endCount;
+	private StringBuffer pagingHtml;
+
+	public Paging(int currentPage, int totalCount, int blockCount,
+			int blockPage, String pageUrl) {
+		this(null, null, currentPage, totalCount, blockCount, blockPage,
+				pageUrl, null);
+	}
+
+	public Paging(int currentPage, int totalCount, int blockCount,
+			int blockPage, String pageUrl, String addKey) {
+		this(null, null, currentPage, totalCount, blockCount, blockPage,
+				pageUrl, addKey);
+	}
+
+	public Paging(String keyField, String keyWord, int currentPage,
+			int totalCount, int blockCount, int blockPage, String pageUrl) {
+		this(keyField, keyWord, currentPage, totalCount, blockCount, blockPage,
+				pageUrl, null);
+	}
+
+	public Paging(String keyField, String keyWord, int currentPage,
+			int totalCount, int blockCount, int blockPage, String pageUrl,
+			String addKey) {
+		if (addKey == null) {
+			addKey = "";
+		}
+		int totalPage = (int)Math.ceil((double)totalCount/blockCount);
+		if (totalPage == 0) {
+			totalPage = 1;
+		}
+		if (currentPage > totalPage) {
+			currentPage = totalPage;
+		}
+		this.startCount = ((currentPage - 1) * blockCount + 1);
+		this.endCount = (currentPage * blockCount);
+
+		int startPage = (currentPage - 1) / blockPage * blockPage + 1;
+		int endPage = startPage + blockPage - 1;
+		if (endPage > totalPage) {
+			endPage = totalPage;
+		}
+		this.pagingHtml = new StringBuffer();		
+
+		if (currentPage > blockPage) {
+			if (keyWord == null) {
+				this.pagingHtml.append("<a href=" + pageUrl + "?pageNum="
+						+ (startPage - 1) + addKey + ">");
+			} else {
+				this.pagingHtml.append("<a href=" + pageUrl + "?keyField="
+						+ keyField + "&keyWord=" + keyWord + "&pageNum="
+						+ (startPage - 1) + addKey + " style='text-decoration: none;'>");
+			}
+			this.pagingHtml.append("<font size='2'><</font>");
+			this.pagingHtml.append("</a>");
+		}
+		
+		this.pagingHtml.append("&nbsp;&nbsp;");
+		for (int i = startPage; i <= endPage; i++) {
+			if (i > totalPage) {
+				break;
+			}
+			if (i == currentPage) {
+				this.pagingHtml.append("&nbsp;<b> <font color='red'>");
+				this.pagingHtml.append(i);
+				this.pagingHtml.append("</font></b>");
+			} else {
+				if (keyWord == null) {
+					this.pagingHtml.append("&nbsp;<a href='" + pageUrl
+							+ "?pageNum=");
+				} else {
+					this.pagingHtml.append("&nbsp;<a href='" + pageUrl
+							+ "?keyField=" + keyField + "&keyWord=" + keyWord
+							+ "&pageNum=");
+				}
+				this.pagingHtml.append(i);
+				this.pagingHtml.append(addKey + "'style='text-decoration: none; font-size:12px;'>");
+				this.pagingHtml.append(i);
+				this.pagingHtml.append("</a>");
+			}
+			this.pagingHtml.append("&nbsp;");
+		}
+		this.pagingHtml.append("&nbsp;&nbsp;&nbsp;&nbsp;");
+		if (totalPage - startPage >= blockPage) {
+			if (keyWord == null) {
+				this.pagingHtml.append("<a href=" + pageUrl + "?pageNum="
+						+ (endPage + 1) + addKey + ">");
+			} else {
+				this.pagingHtml.append("<a href=" + pageUrl + "?keyField="
+						+ keyField + "&keyWord=" + keyWord + "&pageNum="
+						+ (endPage + 1) + addKey +" style='text-decoration: none;'>");
+			}
+			this.pagingHtml.append("<font size='2'>></font>");
+			this.pagingHtml.append("</a>");
+		}
+		
+	}
+
+	public StringBuffer getPagingHtml() {
+		return this.pagingHtml;
+	}
+
+	public int getStartCount() {
+		return this.startCount;
+	}
+
+	public int getEndCount() {
+		return this.endCount;
+	}
+}
